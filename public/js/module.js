dec62fd88764781ce0d42b7c7f11f33cc761eebc
Jovvi Johnson <jjohnsow@gmail.com>
2016-04-12 20:46:19 -0700

    added photo and more states

--- public/js/module.js
+++ public/js/module.js
@@ -1,10 +1,12 @@
 'use strict';
 
-var app = angular.module('myApp', ['ui.router']);
+var app = angular.module('myApp', ['ui.router', 'ngAnimate']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
-    .state('home', {url: '/', templateUrl: '/html/home.html'})
+    .state('home', {url: '/home', templateUrl: '/html/home.html'})
+    .state('about', {url: '/about', templateUrl: '/html/about.html'})
+    .state('resume', {url: '/resume', templateUrl: '/html/resume.html'})
     .state('portfolio', {url: '/portfolio', templateUrl: '/html/portfolio.html', controller: 'PortfolioCtrl'})
   $urlRouterProvider.otherwise('/');
 });
